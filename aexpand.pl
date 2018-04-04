45527776376b3f261cd267c555dc845a11d5928c
rokicki <rokicki@gmail.com>
2018-04-04 12:03:24 -0700

    Added a Perl script that converts axial-formatted move sequences to more conventional HTM-style move sequences.

---
+++ aexpand.pl
@@ -0,0 +1,21 @@
+#
+#   Expand axial-turn metric solutions
+#
+#   U F R D B L
+my @moves = ('U','F','R','D','B','L', 'A','C','E','G','H','I', 'J','K','M') ;
+for ($i=0; $i<@moves; $i++) {
+   $mind{$moves[$i]} = $i ;
+}
+sub expand {
+   my $mov = shift ;
+   my $fac = substr($mov, 0, 1) ;
+   my $cnt = substr($mov, 1) ;
+   return $mov if $mind{$fac} < 6 ;
+   my $m1 = $moves[$mind{$fac} % 3] . $cnt ;
+   my $m2 = $moves[$mind{$fac} % 3 + 3] . (1+int(($mind{$fac}-6)/3)) ;
+   return $m1 . $m2 ;
+}
+while (<>) {
+   s/([A-Z][123])/expand($1)/ge ;
+   print ;
+}
