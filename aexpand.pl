712fe9fa4a3fdcf23a7be8b50003470e66a8a77f
rokicki <rokicki@gmail.com>
2018-04-05 10:36:30 -0700

    Make aexpand work for both versions of axial move names for now.

--- aexpand.pl
+++ aexpand.pl
@@ -2,10 +2,13 @@
 #   Expand axial-turn metric solutions
 #
 #   U F R D B L
-my @moves = ('U','F','R','D','B','L', 'A','C','E','G','H','I', 'J','K','M') ;
+my @moves = ('U','F','R','D','B','L', 'A','C','E','G','H','N','O','P','M') ;
 for ($i=0; $i<@moves; $i++) {
    $mind{$moves[$i]} = $i ;
 }
+$mind{'I'} = $mind{'N'} ;
+$mind{'J'} = $mind{'O'} ;
+$mind{'K'} = $mind{'P'} ;
 sub expand {
    my $mov = shift ;
    my $fac = substr($mov, 0, 1) ;
